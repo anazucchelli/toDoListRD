9f1edfb18ddabaf1a1680966b6bcbfe8ae4e51fc
Treinamento <Treinamento@RD723938.raiadrogasil.com>
2020-02-05 17:36:01 -0300

    Inserindo as tarefas

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let btnAddTarefa = document.querySelector('#adicionar-tarefa')
+let tarefas = document.querySelector('#tarefas')
+let inputTarefa = document.querySelector('#tarefa-digitada')
+
+btnAddTarefa.onclick = function(){
+
+    let valorDigitado = inputTarefa.value
+    let tarefaNova = `<div class="col-md-4">
+    <div class="card-tarefa">
+        <div class="texto-tarefa">
+            ${valorDigitado}
+        </div>
+        <div class="botao-tarefa">
+            <img src="images/check.png" width="33" alt="Botão para finalizar tarefa" title="Botão para finalizar tarefa">
+        </div>
+    </div>
+    </div>`
+
+
+tarefas.innerHTML += tarefaNova
+}
+
